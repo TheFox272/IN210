fb13ff1a8fd7fcb7e71d57766d99c2b6048896b1
TheFox272 <fabien.girard27@gmail.com>
2024-02-09 10:40:39 +0100

    first commit

---
+++ Makefile
@@ -0,0 +1,22 @@
+CC = g++
+CFLAGS = -Wall -Werror -std=c++20
+INC = include/
+LIBS = -lm -lsfml-graphics -lsfml-window -lsfml-system
+SRC = src/
+BUILD = build/
+EXEC = main
+
+
+all: $(BUILD)$(EXEC)
+
+$(BUILD)$(EXEC): $(BUILD)main.o $(BUILD)game.o $(BUILD)display.o
+	$(CC) $(CFLAGS) -g $^ -o $@ $(LIBS)
+
+$(BUILD)%.o: $(SRC)%.cpp $(INC)%.hpp
+	mkdir -p $(dir $@)
+	$(CC) $(CFLAGS) -I $(INC) -g -c $< -o $@
+
+.PHONY: clean
+clean:
+	rm -r $(BUILD)
+	
---
+++ tp2/main.c
@@ -0,0 +1,40 @@
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdarg.h>
+#include <stddef.h>
+#include <setjmp.h>
+#include "cmocka.h"
+#include <string.h>
+#include "ratp.h"
+
+/* A test case that does nothing and succeeds. */
+
+
+
+static int setup(void **state) {
+    (void) state;
+    printf("setUp");
+ return 0;
+}
+static int teardown(void **state) {
+    (void) state;
+    printf("tearDown");
+     return 0;
+}
+static void null_test_success(void **state) {
+    (void) state;
+}
+
+static void testComputePrice(void **)
+
+
+
+int main(void) {
+    const struct CMUnitTest tests[] = {
+        cmocka_unit_test(null_test_success),
+        assert_float_equal (float  expected, float actual, float epsilon),
+    };
+ //   cmocka_set_message_output(CM_OUTPUT_XML);
+    return cmocka_run_group_tests_name("toto",tests, setup, teardown);
+}
